1fce0a9bea64862f12364c3b688d51254a2f944d
wallac21 <wallac21@wwu.edu>
2020-04-16 17:53:47 -0700

    added expand file

---
+++ ush/expand.c
@@ -0,0 +1,11 @@
+/*  Cameron Wallace
+ *  April 14, 2020
+ *  CSCI 347 Spring 2020
+ *  Assignment 2
+ */
+
+#include <stdio.h>
+
+/* Prototypes */
+
+int expand (char *orig, char *new, int newsize);
