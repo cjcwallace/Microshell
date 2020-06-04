50431915611e9f77a3964e2b54666924b70a476c
cameron wallace <wallac21@wwu.edu>
2020-06-03 23:47:07 -0700

    Split function working. Set up creation of pthreads. Need to figure out how to use index of split function to assign calculations.

--- mm/pt-mm.c
+++ mm/pt-mm.c
@@ -11,6 +11,7 @@
 #include <string.h>
 #include <errno.h>
 #include <time.h>
+#include <pthread.h>
 
 /* idx macro calculates the correct 2-d based 1-d index
  * of a location (x,y) in an array that has col columns.
@@ -19,16 +20,51 @@
 
 #define idx(x,y,col)  ((x)*(col) + (y))
 
-/* Matrix Multiply:
- *  C (x by z)  =  A ( x by y ) times B (y by z)
- *  This is the slow n^3 algorithm
- *  A and B are not be modified
- */
+struct group {
+  double *A;
+  double *B;
+  double *C;
+  int x;
+  int y;
+  int z;
+  int n; /* number of comps */
+  int threadn;
+};
 
-void MatMul (double *A, double *B, double *C, int x, int y, int z)
+/* Returns list containing number of computations each thread computes
+ * x <- (x * y), tcount <- number of threads used */
+int getSplit(int x, int tcount, int *tsplit)
 {
-  int ix, jx, kx;
+  int v = x / tcount;
+  for( int i = 0; i < tcount; i++ )
+    {
+      tsplit[i] = v;
+    }
+  int excess = x % tcount;
+  if ( excess > 0 )
+    {
+      for( int i = 0; i < excess; i++ )
+	{
+	  tsplit[i] += 1;
+	} 
+    }
+  return 0;
+}
 
+/* idx = (x)(col) + y */
+void * mul_thread (void *arg)
+{
+  struct group *g = (struct group*)arg;
+  double *A = g->A;
+  double *B = g->B;
+  double *C = g->C;
+  int x = g->x;
+  int y = g->y;
+  int z = g->z;
+  int start = g->threadn * g->n;
+  int end = (g->threadn * g->n) + g->n;
+  int ix, jx, kx;
+  
   for (ix = 0; ix < x; ix++) {
     // Rows of solution
     for (jx = 0; jx < z; jx++) {
@@ -43,26 +79,54 @@ void MatMul (double *A, double *B, double *C, int x, int y, int z)
   }
 }
 
+/* Matrix Multiply:
+ *  C (x by z)  =  A ( x by y ) times B (y by z)
+ *  This is the slow n^3 algorithm
+ *  A and B are not be modified
+ */
+void MatMul (double *A, double *B, double *C, int x, int y, int z, int tcount)
+{
+  pthread_t ids[tcount];
+  int tsplit[tcount];
+  getSplit((x * z), tcount, tsplit);
+  for (int i = 0; i < tcount; i++) {
+    struct group *g = malloc(sizeof(struct group));
+    g->threadn = i;
+    g->A = A;
+    g->B = B;
+    g->C = C;
+    g->x = x;
+    g->y = y;
+    g->z = z;
+    g->n = tsplit[i];
+    int err = pthread_create (&ids[i], NULL, mul_thread, (void *) g);
+    if (err) {
+      fprintf(stderr, "Can't create thread %d\n", i);
+      exit(1);
+    }
+  }  
+}
+
 /* Matrix Square: 
  *  B = A ^ 2*times
  *
  *    A are not be modified.
  */
 
-void MatSquare (double *A, double *B, int x, int times)
+void MatSquare (double *A, double *B, int x, int times, int tcount)
 {
   int i;
 
-  MatMul (A, A, B, x, x, x);
+  MatMul (A, A, B, x, x, x, tcount);
   if (times > 1) {
     /* Need a Temporary for the computation */
     double *T = (double *)malloc(sizeof(double)*x*x);
     for (i = 1; i < times; i+= 2) {
-      MatMul (B, B, T, x, x, x);
+      MatMul (B, B, T, x, x, x, tcount);
       if (i == times - 1)
 	memcpy(B, T, sizeof(double)*x*x);
       else
-	MatMul (T, T, B, x, x, x);
+	MatMul (T, T, B, x, x, x, tcount);
     }
     free(T);
   }
@@ -81,7 +145,6 @@ void MatPrint (double *A, int x, int y)
   }
 }
 
-
 /* Generate data for a matrix: */
 void MatGen (double *A, int x, int y, int rand)
 {
@@ -97,14 +160,12 @@ void MatGen (double *A, int x, int y, int rand)
 }
   
 /* Print a help message on how to run the program */
-
 void usage(char *prog)
 {
-  fprintf (stderr, "%s: [-dr] -x val -y val -z val\n", prog);
-  fprintf (stderr, "%s: [-dr] -s num -x val\n", prog);
+  fprintf (stderr, "%s: [-dr] -x val -y val -z val -n num_threads\n", prog);
+  fprintf (stderr, "%s: [-dr] -s num -x val -n num_threads\n", prog);
   exit(1);
 }
-
 
 /* Main function
  *
@@ -114,7 +175,9 @@ void usage(char *prog)
  *         -x   -- rows of the first matrix, r & c for squaring
  *         -y   -- cols of A, rows of B
  *         -z   -- cols of B
- *         
+ *         -n   -- num threads
+ *         -t   -- calc time
+ *  
  */
 
 int main (int argc, char ** argv)
@@ -128,11 +191,16 @@ int main (int argc, char ** argv)
   int square = 0;
   int useRand = 0;
   int sTimes = 0;
+  int tcount = 8;
+  //int t = 0;
   
   while ((ch = getopt(argc, argv, "drs:x:y:z:")) != -1) {
     switch (ch) {
     case 'd':  /* debug */
       debug = 1;
+      break;
+    case 'n': /* num threads */
+      tcount = atoi(optarg);
       break;
     case 'r':  /* debug */
       useRand = 1;
@@ -141,7 +209,10 @@ int main (int argc, char ** argv)
     case 's':  /* s times */
       sTimes = atoi(optarg);
       square = 1;
-      break;
+      break;/*
+    case 'T':
+      t = 1;
+      break;*/
     case 'x':  /* x size */
       x = atoi(optarg);
       break;
@@ -177,7 +248,7 @@ int main (int argc, char ** argv)
     A = (double *) malloc (sizeof(double) * x * x);
     B = (double *) malloc (sizeof(double) * x * x);
     MatGen(A,x,x,useRand);
-    MatSquare(A, B, x, sTimes);
+    MatSquare(A, B, x, sTimes, tcount);
     if (debug) {
       printf ("-------------- orignal matrix ------------------\n");
       MatPrint(A,x,x);
@@ -190,7 +261,7 @@ int main (int argc, char ** argv)
     C = (double *) malloc (sizeof(double) * x * z);
     MatGen(A,x,y,useRand);
     MatGen(B,y,z,useRand);
-    MatMul(A, B, C, x, y, z);
+    MatMul(A, B, C, x, y, z, tcount);
     if (debug) {
       printf ("-------------- orignal A matrix ------------------\n");
       MatPrint(A,x,y);
